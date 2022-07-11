5d4757ea8799e70d9204b09367d2e7726131e033
Simon Ansell <sansell@hublsoft.com>
2022-07-11 12:39:55 +0100

    3.3 complete

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Working through https://livebook.manning.com/book/d3js-in-action-third-edition
 - Why is there a primitive `<circle />` element, isn't `<ellipse />` sufficient? Compare that there is no `<square />` element. Same for `<line />`.
 
 - a rule of thumb that is generally accepted is that we should consider using canvas rather than SVG if a visualization contains more than 1000 elements.
+
+### Chapter 3 Data
+
+- "Discrete data consists of whole numbers, also called integers, that cannot be subdivided.". What? Discrete data is not necessarily integers.
--- src/chapter-3/Chapter3.jsx
+++ src/chapter-3/Chapter3.jsx
@@ -3,12 +3,46 @@ import * as d3 from "d3";
 
 import "./chapter3.css"
 
+const dataURI = "https://raw.githubusercontent.com/d3js-in-action-third-edition/code-files/main/chapter_03/3.2-Preparing_data/start/data/data.csv"
+
 const Chapter2 = () => {
     useEffect(() => {
-        const svg = d3.select(".responsive-svg-container")
-            .append("svg")
-                .attr("viewBox", "0 0 1200 1600")
-                .style("border", "1px solid black")
+        let svg;
+
+        const createViz = vizData => {
+            const barHeight = 20;
+            const padding = 5;
+
+            svg
+                .selectAll("rect")
+                .data(vizData)
+                .join("rect")
+                    .attr("class", d => {
+                        console.log(d)
+                        return "bar"
+                    })
+                    .attr("width", d => d.count)
+                    .attr("height", barHeight)
+                    .attr("y", (d, i) => (barHeight  + padding) * i)
+                    .attr("fill", d => d.technology === "D3.js" ? "red" : "#bada55")
+        }
+
+        const init = async () => {     
+            svg = d3.select(".responsive-svg-container")
+                .append("svg")
+                    .attr("viewBox", "0 0 1200 1600")
+                    // .style("border", "1px solid black")
+
+            const data = await d3.csv(dataURI, d => ({
+                technology: d.technology,
+                count: +d.count, // Why the + operator and not Number(d.count) which I think is more explicit?
+            }))
+            
+            const sortedData = data.sort((a, b) => b.count - a.count)
+            createViz(sortedData)
+        }
+
+        init();
     }, [])
 
     return (
