ae1fc71ce4d35fbddc8ffadece718272277a3f89
Simon Ansell <sansell@hublsoft.com>
2022-07-11 13:24:11 +0100

    WIP

--- src/chapter-3/Chapter3.jsx
+++ src/chapter-3/Chapter3.jsx
@@ -3,7 +3,8 @@ import * as d3 from "d3";
 
 import "./chapter3.css"
 
-const dataURI = "https://raw.githubusercontent.com/d3js-in-action-third-edition/code-files/main/chapter_03/3.2-Preparing_data/start/data/data.csv"
+// const dataURI = "https://raw.githubusercontent.com/d3js-in-action-third-edition/code-files/main/chapter_03/3.2-Preparing_data/start/data/data.csv"
+const dataURI = "https://raw.githubusercontent.com/JaseZiv/worldfootballR_data/master/raw-data/all_leages_and_cups/all_competitions.csv"
 
 const Chapter2 = () => {
     useEffect(() => {
@@ -33,13 +34,20 @@ const Chapter2 = () => {
                     .attr("viewBox", "0 0 1200 1600")
                     // .style("border", "1px solid black")
 
-            const data = await d3.csv(dataURI, d => ({
-                technology: d.technology,
-                count: +d.count, // Why the + operator and not Number(d.count) which I think is more explicit?
-            }))
+            const data = await d3.csv(dataURI, d => {
+                // return {
+                //     competition_name: d.competition_name,
+                // }
+                return d
+            })
+
+            console.log(data.filter(d => d.competition_name === "FA Cup"))
+
+            // const distinctValues = [...new Set(data.map((d) => d.competition_name))].sort()
+            // console.log(distinctValues)
             
-            const sortedData = data.sort((a, b) => b.count - a.count)
-            createViz(sortedData)
+            // const sortedData = data.sort((a, b) => b.count - a.count)
+            // createViz(sortedData)
         }
 
         init();
