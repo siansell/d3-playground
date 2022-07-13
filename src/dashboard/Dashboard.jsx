99193ea7afbe873a67e6fadd7e96d71d0454b808
Simon Ansell <sansell@hublsoft.com>
2022-07-13 14:25:49 +0100

    switch y-axis

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,172 +1,256 @@
-import { useEffect } from "react"
+import { useEffect, useState, useRef } from "react"
 import * as d3 from "d3";
 
 import "./dashboard.css"
 
 const data = [
     {
-        id: 1,
-        name: "Factory A",
-        values: {
-            maintenance: 750,
-            salary: 3000,
-            rent: 1200,
-            size: 22 , // m2
-            profitMargin: 5, // %
+        id: '10',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1200,
+            size:25,
+            profitMargin:7
         }
     },
     {
-        id: 2,
-        name: "Factory B",
-        values: {
-            maintenance: 1000,
-            salary: 5000,
-            rent: 1400,
-            size: 44 , // m2
-            profitMargin: 10, // %
+        id: '11',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1000,
+            size:18,
+            profitMargin:9
         }
     },
     {
-        id: 3,
-        name: "Factory C",
-        values: {
-            maintenance: 400,
-            salary: 1234,
-            rent: 800,
-            size: 123 , // m2
-            profitMargin: 22, // %
+        id: '12',
+        values:{
+            maintenance:3000,
+            salary:6000,
+            rent:2000,
+            size:25,
+            profitMargin:-2.5
         }
     },
+    {
+        id: '13',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:700,
+            size:15,
+            profitMargin:11.75
+        }
+    },
+    {
+        id: '14',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1800,
+            size:29,
+            profitMargin:7
+        }
+    },
+    {
+        id: '15',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:600,
+            size:13,
+            profitMargin:14
+        }
+    },
+    {
+        id: '16',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1600,
+            size:28,
+            profitMargin:5
+        }
+    },
+    {
+        id: '17',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1200,
+            size:25,
+            profitMargin:8
+        }
+    },
+    {
+        id: '18',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1100,
+            size:25,
+            profitMargin:4
+        }
+    },
+    {
+        id: '19',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:1200,
+            size:25,
+            profitMargin:12
+        }
+    },
+    {
+        id: '20',
+        values:{
+            maintenance:1200,
+            salary:5000,
+            rent:800,
+            size:15,
+            profitMargin:10.25
+        }
+    }
 ]
 
 const Dashboard = () => {
-    useEffect(() => {
-        const drawStackedBarChart = () => {
-            const margin = {top: 40, right: 170, bottom: 25, left: 40};
-            const width = 500;
-            const height = 500;
-            const innerWidth = width - margin.left - margin.right;
-            const innerHeight = height - margin.top - margin.bottom;
-            
-            const svg = d3.select("#stacked-bar-chart")
-                .append("svg")
-                // .attr("viewBox", `0, 0, ${width}, ${height}`)
-                .style("border", "1px solid black")
-                .style("height", "500px")
-                .style("width", "500px")
-            
-            const innerChart = svg
-                .append("g")
-                .attr("transform", `translate(${margin.left}, ${margin.top})`)     
-            
-            // y-axis
-            const maxCost = d3.max(data, d => d.values.maintenance + d.values.salary + d.values.rent);
-            const yScale = d3.scaleLinear()
-                .domain([0, maxCost])
-                .range([innerHeight, 0]);
-            const leftAxis = d3.axisLeft(yScale); 
-            innerChart
-                .append("g")
-                .attr("class", "axis-y")
-                .call(leftAxis);
+    const [mode, setMode] = useState('absolute') // || percentage
+    const isInitialisedRef = useRef(false)
 
-            // x-axis
-            const xScale = d3.scaleBand()
-                .domain(data.map(d => d.name))
-                .range([0, innerWidth])
-            const bottomAxis = d3.axisBottom(xScale);
-            innerChart
-                .append("g")
-                .attr("class", "axis-x")
-                .call(bottomAxis)
-                .attr('transform', `translate(0, ${innerHeight})`);
+    const innerChart = useRef(null)
+    const yScale = useRef(null);
 
-            // bars
-            const barWidth = xScale.bandwidth() / 2;
+    const margin = {top: 40, right: 170, bottom: 25, left: 40};
+    const width = 500;
+    const height = 500;
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
-            // maintenance
-            innerChart.append('g').classed('bars', true)
-            innerChart.select('.bars').selectAll('g')
-                .data(data)
-                .join('g')
-                .attr('class', d => `bar bar-${d.name}`)
-                .each(function(d) {
-                    // maintenance
-                    d3.select(this)
-                        .append('rect')
-                        .attr('class', 'bar-maintenance')
-                        .attr('x', d => xScale(d.name) + barWidth /2 )
-                        .attr('y', d => yScale(d.values.maintenance))
-                        .attr('height', d => innerHeight - yScale(d.values.maintenance))
-                        .attr('fill', '#ff0000')
-                        .attr('width', barWidth)
+    const init = () => {
+        const svg = d3.select("#stacked-bar-chart")
+            .append("svg")
+            // .attr("viewBox", `0, 0, ${width}, ${height}`)
+            .style("border", "1px solid black")
+            .style("height", "500px")
+            .style("width", "500px")
+        
+        innerChart.current = svg
+            .append("g")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`)     
+        
+        // y-axis
+        if (mode === 'absolute') {
+        const maxCost = d3.max(data, d => d.values.maintenance + d.values.salary + d.values.rent);
+        yScale.current = d3.scaleLinear()
+            .domain([0, maxCost])
+            .range([innerHeight, 0]);
+        } else {
+            yScale.current = d3.scaleLinear()
+            .domain([0, 100])
+            .range([innerHeight, 0]);
+        }
+        
+        const leftAxis = d3.axisLeft(yScale.current); 
+        innerChart.current
+            .append("g")
+            .attr("class", "axis-y")
+            .call(leftAxis);
 
-                        // salary
-                    d3.select(this)
-                        .append('rect')
-                        .attr('class', 'bar-salary')
-                        .attr('x', d => xScale(d.name) + barWidth /2 )
-                        .attr('y', d => yScale(d.values.salary + d.values.maintenance))
-                        .attr('height', d => innerHeight - yScale(d.values.salary))
-                        .attr('fill', 'blue')
-                        .attr('width', barWidth)
+// x-axis
+const xScale = d3.scaleBand()
+    .domain(data.map(d => d.id))
+    .range([0, innerWidth])
+const bottomAxis = d3.axisBottom(xScale);
+innerChart.current
+    .append("g")
+    .attr("class", "axis-x")
+    .call(bottomAxis)
+    .attr('transform', `translate(0, ${innerHeight})`);
 
-                    // rent
-                    d3.select(this)
-                    .append('rect')
-                    .attr('class', 'bar-rent')
-                    .attr('x', d => xScale(d.name) + barWidth /2 )
-                    .attr('y', d => yScale(d.values.salary + d.values.rent + d.values.maintenance))
-                    .attr('height', d => innerHeight - yScale(d.values.rent))
-                    .attr('fill', 'green')
-                    .attr('width', barWidth)
-                })
+// bars
+const barWidth = xScale.bandwidth() / 2;
 
-/*
-            innerChart.selectAll('rect.maintenance')
-                .classed('maintenance', true)
-                .data(data)
-                .join('rect')
-                .attr('x', d => xScale(d.name) + barWidth /2 )
-                .attr('y', d => innerHeight - yScale(d.values.maintenance))
-                .attr('width', barWidth)
-                .attr('height', d => yScale(d.values.maintenance))
-                .attr('fill', '#ff0000')
+// maintenance
+innerChart.current.append('g').classed('bars', true)
+innerChart.current.select('.bars').selectAll('g')
+    .data(data)
+    .join('g')
+    .attr('class', d => `bar bar-${d.id}`)
+    .each(function(d) {
+        // maintenance
+        d3.select(this)
+            .append('rect')
+            .attr('class', 'bar-maintenance')
+            .attr('x', d => xScale(d.id) + barWidth /2 )
+            .attr('y', d => yScale.current(d.values.maintenance))
+            .attr('height', d => innerHeight - yScale.current(d.values.maintenance))
+            .attr('fill', '#ff0000')
+            .attr('width', barWidth)
 
             // salary
-            innerChart.selectAll('rect.salary')
-                .classed('salary', true)
-                .data(data)
-                .join('rect')
-                .attr('x', d => xScale(d.name) + barWidth /2 )
-                .attr('y', d => innerHeight - yScale(d.values.salary + d.values.maintenance))
-                .attr('width', barWidth)
-                .attr('height', d => yScale(d.values.salary))
-                .attr('fill', 'blue')
+        d3.select(this)
+            .append('rect')
+            .attr('class', 'bar-salary')
+            .attr('x', d => xScale(d.id) + barWidth /2 )
+            .attr('y', d => yScale.current(d.values.salary + d.values.maintenance))
+            .attr('height', d => innerHeight - yScale.current(d.values.salary))
+            .attr('fill', 'blue')
+            .attr('width', barWidth)
 
-            // rent
-            innerChart.selectAll('rect.salary')
-                .classed('rent', true)
-                .data(data)
-                .join('rect')
-                .attr('x', d => xScale(d.name) + barWidth /2 )
-                .attr('y', d => innerHeight - yScale(d.values.salary + d.values.maintenance + d.values.rent))   
-                .attr('width', barWidth)
-                .attr('height', d => yScale(d.values.rent))
-                .attr('fill', 'green')
-                */
-        }
-        
-        const init = async () => {     
-            drawStackedBarChart()
-        }
-        
+        // rent
+        d3.select(this)
+        .append('rect')
+        .attr('class', 'bar-rent')
+        .attr('x', d => xScale(d.id) + barWidth /2 )
+        .attr('y', d => yScale.current(d.values.salary + d.values.rent + d.values.maintenance))
+        .attr('height', d => innerHeight - yScale.current(d.values.rent))
+        .attr('fill', 'green')
+        .attr('width', barWidth)
+    })            
+
+        isInitialisedRef.current = true;
+    }
+
+    const update = () => {
+        if (mode === 'absolute') {
+            const maxCost = d3.max(data, d => d.values.maintenance + d.values.salary + d.values.rent);
+            yScale.current = d3.scaleLinear()
+                .domain([0, maxCost])
+                .range([innerHeight, 0]);
+            } else {
+                yScale.current = d3.scaleLinear()
+                .domain([0, 100])
+                .range([innerHeight, 0]);
+            }
+
+            const leftAxis = d3.axisLeft(yScale.current); 
+            innerChart.current
+                .select("g.axis-y")
+                .call(leftAxis);
+    }
+
+    useEffect(() => {
+        if (!isInitialisedRef.current) return
+        update();
+    }, [mode])
+
+    useEffect(() => {
         init();
     }, [])
+
+    const handleSetMode =() => {
+        setMode(mode === 'absolute' ? 'percentage' : 'absolute')
+    }
     
     return (
             <>
                 <h1>hubl Dashboard</h1>
+                <button onClick={handleSetMode} style={{ marginBottom: 10 }}>
+                    {mode}
+                </button>
                 <div id="stacked-bar-chart"></div>
             </>
         )
